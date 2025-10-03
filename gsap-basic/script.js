dbb3758369d20e33c4b36be1cfbef6fbbfbaafdd
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-03 21:35:57 +0600

    using stagger property

--- gsap-basic/script.js
+++ gsap-basic/script.js
@@ -12,11 +12,20 @@
 // })
 
 
-gsap.to('#main',{
-    x:1600,
-    duration:5,
-    delay:2,
-    rotate:360,
-    backgroundColor:'yellow',
-    borderRadius:"100%"
+// gsap.to('#main',{
+//     x:1600,
+//     duration:5,
+//     delay:2,
+//     rotate:360,
+//     backgroundColor:'yellow',
+//     borderRadius:"100%"
+// })
+
+
+gsap.from('h1',{
+    y:100,
+    opacity:0,
+    duration:1.5,
+    delay:1,
+    stagger:0.3
 })
