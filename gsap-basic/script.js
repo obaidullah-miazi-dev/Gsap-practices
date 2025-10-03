744018874a1abdec3d75b0a6edd902c769728c31
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-03 21:21:04 +0600

    gsap from

--- gsap-basic/script.js
+++ gsap-basic/script.js
@@ -1,5 +1,12 @@
-gsap.to('#main',{
-    x:1600,
-    duration:5,
-    delay:3 // after 3sec start animation
-})
+// gsap.to('#main',{
+//     x:1600,
+//     duration:5,
+//     delay:3 // after 3sec start animation
+// })
+
+// gsap.from('#main',{
+//     x:1600,
+//     duration:5,
+//     delay:1,
+//     rotate:360
+// })
