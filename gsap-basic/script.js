1665a646995ee68eb784592a6adf961de18d7ee7
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-03 21:49:52 +0600

    repeat and yoyo property used

--- gsap-basic/script.js
+++ gsap-basic/script.js
@@ -22,10 +22,20 @@
 // })
 
 
-gsap.from('h1',{
-    y:100,
-    opacity:0,
-    duration:1.5,
+// gsap.from('h1',{
+//     y:100,
+//     opacity:0,
+//     duration:1.5,
+//     delay:1,
+//     stagger:0.3
+// })
+
+gsap.to('#box1',{
+    x:1600,
+    duration:3,
     delay:1,
-    stagger:0.3
+    rotate:360,
+    // repeat:1 // its mean repeat the animation 1 time
+    repeat:-1, // its mean playing the animation infinit
+    yoyo:true, // this property work like yoyo...
 })
