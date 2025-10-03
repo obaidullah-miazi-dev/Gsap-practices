574ad4117ab1ad1f01a1fe24016aa7ad0756c763
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-03 21:26:30 +0600

    in gsap used css property

--- gsap-basic/script.js
+++ gsap-basic/script.js
@@ -10,3 +10,13 @@
 //     delay:1,
 //     rotate:360
 // })
+
+
+gsap.to('#main',{
+    x:1600,
+    duration:5,
+    delay:2,
+    rotate:360,
+    backgroundColor:'yellow',
+    borderRadius:"100%"
+})
